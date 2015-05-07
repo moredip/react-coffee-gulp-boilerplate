577af5cfc9eb53d421a8d45d2ff75d6a2a705d92
Pete Hodgson <git@thepete.net>
2015-05-07 13:02:30 -1000

    more cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,6 @@ var gulp = require('gulp'),
 
 var BUILD_DIR = 'build';
 
-function handleError(err) {
-  console.log(err.toString());
-  this.emit('end');
-}
-
 gulp.task('clean', function(cb){
   del([BUILD_DIR],cb);
 });
@@ -42,25 +37,6 @@ var createBrowserifyTask = function(taskName, entryFile){
 };
 
 createBrowserifyTask('browserify','./src/app.coffee');
-
-//createBrowserifyTask('build-feature-test-harness','./tests/feature/app_test_harness.js');
-
-//gulp.task('unit-test', function() {
-  //gulp.src(['tests/setup.js','tests/unit/**/*.js'],{read:false})
-    //.pipe(mocha({
-      //ui: 'bdd',
-      //useColors: false,
-    //}))
-    //.on("error", handleError);
-//});
-
-//gulp.task('sass', function () {
-    //gulp.src('scss/*.scss')
-        //.pipe(sass({
-          //includePaths: require('node-bourbon').includePaths.concat( FONT_AWESOME_INCLUDE_PATH )
-        //}))
-        //.pipe(gulp.dest(BUILD_DIR));
-//});
 
 gulp.task('build',['copy','browserify']);
 
